3b9e4c7a66cd71b3b08461204b152a2738273977
Thorsten Heit <thorsten.heit@vkb.de>
2011-06-30 10:48:38 +0200

    Packages org.eclipse.m2e.axistools renamed to de.theit.m2e.axistools

--- de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenProjectConfigurator.java
+++ de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenProjectConfigurator.java
@@ -7,7 +7,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.m2e.axistools.internal;
+package de.theit.m2e.axistools.internal;
 
 import java.io.File;
 
