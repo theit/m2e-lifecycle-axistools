4e72b0b8626b715a4bfe9c5ca215d41fd4b45a9e
Thorsten Heit <theit@gmx.de>
2012-09-03 16:27:57 +0200

    - Fixed incorrect license header in Java sources - Removed AxistoolsCodeGenProjectConfigurator#getSourceFolders() - Updated plugin version to 1.1.2

--- de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenBuildParticipant.java
+++ de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenBuildParticipant.java
@@ -1,11 +1,18 @@
 /*
  * @(#)AxistoolsCodeGenBuildParticipant.java
- * Copyright (C)2011 Thorsten Heit
+ * Copyright (C)2011-2012 Thorsten Heit
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This file is licensed to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package de.theit.m2e.axistools.internal;
 
@@ -28,39 +35,39 @@ import org.sonatype.plexus.build.incremental.BuildContext;
  * @version $Id$
  */
 public class AxistoolsCodeGenBuildParticipant extends
-        MojoExecutionBuildParticipant {
-    /**
-     * Creates a new instance of this class.
-     * 
-     * @param execution
-     *            Mojo execution.
-     */
-    public AxistoolsCodeGenBuildParticipant(MojoExecution execution) {
-        super(execution, true);
-    }
+		MojoExecutionBuildParticipant {
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param execution
+	 *            Mojo execution.
+	 */
+	public AxistoolsCodeGenBuildParticipant(MojoExecution execution) {
+		super(execution, true);
+	}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant#build(int,
-     *      org.eclipse.core.runtime.IProgressMonitor)
-     */
-    @Override
-    public Set<IProject> build(int kind, IProgressMonitor monitor)
-            throws Exception {
-        IMaven maven = MavenPlugin.getMaven();
-        BuildContext buildContext = getBuildContext();
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant#build(int,
+	 *      org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
+		IMaven maven = MavenPlugin.getMaven();
+		BuildContext buildContext = getBuildContext();
 
-        // execute mojo
-        Set<IProject> result = super.build(kind, monitor);
+		// execute mojo
+		Set<IProject> result = super.build(kind, monitor);
 
-        // tell m2e builder to refresh generated files
-        File generated = maven.getMojoParameterValue(getSession(),
-                getMojoExecution(), "outputDirectory", File.class);
-        if (null != generated) {
-            buildContext.refresh(generated);
-        }
+		// tell m2e builder to refresh generated files
+		File generated = maven.getMojoParameterValue(getSession(),
+				getMojoExecution(), "outputDirectory", File.class);
+		if (null != generated) {
+			buildContext.refresh(generated);
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
--- de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenProjectConfigurator.java
+++ de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenProjectConfigurator.java
@@ -1,22 +1,25 @@
 /*
  * @(#)AxistoolsCodeGenProjectConfigurator.java
- * Copyright (C)2011 Thorsten Heit
+ * Copyright (C)2011-2012 Thorsten Heit
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This file is licensed to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package de.theit.m2e.axistools.internal;
 
-import java.io.File;
-
 import org.apache.maven.plugin.MojoExecution;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
-import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
 
 /**
@@ -27,31 +30,18 @@ import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
  * @version $Id$
  */
 public class AxistoolsCodeGenProjectConfigurator extends
-        AbstractJavaProjectConfigurator {
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator#getBuildParticipant(org.eclipse.m2e.core.project.IMavenProjectFacade,
-     *      org.apache.maven.plugin.MojoExecution,
-     *      org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata)
-     */
-    @Override
-    public AbstractBuildParticipant getBuildParticipant(
-            IMavenProjectFacade projectFacade, MojoExecution execution,
-            IPluginExecutionMetadata executionMetadata) {
-        return new AxistoolsCodeGenBuildParticipant(execution);
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator#getSourceFolders(org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest,
-     *      org.apache.maven.plugin.MojoExecution)
-     */
-    @Override
-    protected File[] getSourceFolders(ProjectConfigurationRequest request,
-            MojoExecution mojoExecution) throws CoreException {
-        return getParameterValue("outputDirectory", File[].class,
-                request.getMavenSession(), mojoExecution);
-    }
+		AbstractJavaProjectConfigurator {
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator#getBuildParticipant(org.eclipse.m2e.core.project.IMavenProjectFacade,
+	 *      org.apache.maven.plugin.MojoExecution,
+	 *      org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata)
+	 */
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(
+			IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		return new AxistoolsCodeGenBuildParticipant(execution);
+	}
 }
